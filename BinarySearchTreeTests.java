020541b3fc17e2abf5492ffa8c197d003313d862
hungryglobe8 <46876520+hungryglobe8@users.noreply.github.com>
2019-03-21 14:41:22 -0600

    Add files via upload

--- BinarySearchTreeTests.java
+++ BinarySearchTreeTests.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Random;
 
@@ -16,22 +17,23 @@ class BinarySearchTreeTests {
 
 	private BinarySearchTree<String> sTree = new BinarySearchTree<String>();
 	private BinarySearchTree<Integer> iTree = new BinarySearchTree<Integer>();
-//	
-//	    iList1 = new ArrayList<>(); 
-//		iList2 = new ArrayList<>(); 
-//
-//		for (int i = 0; i <= 10; i++)
-//		{
-//			iList1.add("v" + i);  
-//			iList2.add("v" + (i+1));
-//		}
-//		iList1.add("v" + 5);
-//		iList2.add("v" + 1);
-//		
-//		aList1 = new ArrayList<Character>();
-//		aList2 = new ArrayList<Character>();
-//		aList1.add('a');
-//		aList2.add('a');
+	private ArrayList<Integer> iItems = new ArrayList<Integer>();
+
+	@BeforeEach
+	void setUp() throws Exception
+	{
+		iTree.add(8);
+		iTree.add(3);
+		iTree.add(6);
+		iTree.add(15);
+		iTree.add(12);
+		iTree.add(7);
+		
+		iItems.add(7);
+		iItems.add(15);
+		iItems.add(8);
+		iItems.add(6);
+	}
 	
 	@AfterEach
 	void tearDown() throws Exception {
@@ -40,25 +42,82 @@ class BinarySearchTreeTests {
 	@Test
 	void empty()
 	{
-//		assertTrue(sTree.isEmpty());
+		assertTrue(sTree.isEmpty());
 		sTree.add("Cat");
 		assertFalse(sTree.isEmpty());
 	}
 	
 	@Test
-	void inOrder()
+	void add()
 	{
-//		iTree.add(5);
-//		iTree.add(3);
-//		iTree.add(6);
-//		iTree.add(1);
-//		for(int i = 4; i < 52; i+=3)
-//		{
-//			iTree.add(i);
-//		}
-//		iTree.toArrayList();
+		sTree.add("Human");
+		assertTrue(sTree.contains("Human"));
+		//case sensitive unless comparison is changed, which will be necessary in a future spell-checker class
+		assertFalse(sTree.contains("human"));
 	}
 	
+	@Test
+	void size()
+	{
+		sTree.add("apple");
+		assertEquals(1, sTree.size());
+		
+		assertEquals(6, iTree.size());
+	}
+	
+	@Test
+	void addAll()
+	{
+		BinarySearchTree<Integer> test = new BinarySearchTree<Integer>();
+		test.addAll(iItems);
+		
+		assertEquals(4, test.size());
+	}
+	
+	@Test
+	void inOrder()
+	{
+//		iTree.toArrayList();
+//		System.out.println(iTree);
+	}
+	
+	@Test
+	void contains()
+	{
+		assertTrue(iTree.contains(8));
+		assertTrue(iTree.contains(7));
+		assertFalse(iTree.contains(9));
+		assertTrue(iTree.contains(15));
+	}
+	
+	@Test
+	void containsAll()
+	{
+		
+		ArrayList<Integer> items = new ArrayList<Integer>();
+		items.add(7);
+		items.add(15);
+		items.add(8);
+		items.add(6);
+		
+		assertTrue(iTree.containsAll(items));
+	}
+	
+	@Test
+	void clear()
+	{
+		iTree.clear();
+		assertTrue(iTree.isEmpty());
+	}
+	
+	@Test
+	void remove_Root()
+	{
+//		iItems.remove(8);
+//		System.out.println(iTree);
+//		assertFalse(iTree.contains(8));
+		
+	}
 	@Test
 	void string()
 	{
